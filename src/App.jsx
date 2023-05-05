bec10ef0904f4a907901a0c783675d6c97f3b293
Aswin Andro <aswin.knight@gmail.com>
2023-05-05 14:12:19 +0530

    Revision
    Revision

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Experience from './components/Experience';
+// import Feedbacks from './components/Feedbacks';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+// import StarsCanvas from "./components/StarsCanvas";
+// import Tech from './components/Tech';
+// import Works from './components/Works';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <h1>dsadfdsf</h1>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-centter"></div>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
         <Navbar />
         <Hero />
       </div>
-      <About />
+      {/* <About />
       <Experience />
       <Tech />
       <Works />
-      <Feedbacks />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
+      <Feedbacks /> */}
+      <div className="relative z-0"> 
+        {/* <Contact />
+        <StarsCanvas /> */}
       </div>
     </BrowserRouter>
   );
